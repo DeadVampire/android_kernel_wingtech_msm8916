e1e1db97e65ca9ee05058c184a9101d7a0646c5e
Anik1199 <anik@lineageos.org>
2017-02-08 17:23:18 +0600

    camera_v2: Partial roll back to LA.BR.1.2.4-04410 stack
    Change-Id: Ie9f2e77ea9e1540f959fee32f02d892635f2f044

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.h
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.h
@@ -42,9 +42,9 @@ struct msm_actuator_func_tbl {
 		struct msm_actuator_set_info_t *);
 	int32_t (*actuator_init_focus)(struct msm_actuator_ctrl_t *,
 		uint16_t, struct reg_settings_t *);
-	int32_t (*actuator_set_default_focus)(struct msm_actuator_ctrl_t *,
+	int32_t (*actuator_set_default_focus) (struct msm_actuator_ctrl_t *,
 			struct msm_actuator_move_params_t *);
-	int32_t (*actuator_move_focus)(struct msm_actuator_ctrl_t *,
+	int32_t (*actuator_move_focus) (struct msm_actuator_ctrl_t *,
 			struct msm_actuator_move_params_t *);
 	void (*actuator_parse_i2c_params)(struct msm_actuator_ctrl_t *,
 			int16_t, uint32_t, uint16_t);
